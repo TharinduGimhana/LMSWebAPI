f9510ef4e8af875dbfb452103e8b754b32fbd6ae
TharinduGimhana <gimhana329@gmail.com>
2020-10-16 15:34:24 +0530

    first commit

---
+++ LMSWebAPI/Models/Books.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LMSWebAPI.Models
+{
+    public partial class Books
+    {
+        public int Id { get; set; }
+        public string BookCode { get; set; }
+        public string BookName { get; set; }
+        public int? BookCount { get; set; }
+    }
+}
---
+++ LMSWebAPI/Models/LMSContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace LMSWebAPI.Models
+{
+    public partial class LMSContext : DbContext
+    {
+        public LMSContext()
+        {
+        }
+
+        public LMSContext(DbContextOptions<LMSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Books> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-4R86U0P; Database=LMS; Trusted_Connection=True; MultipleActiveResultSets=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Books>(entity =>
+            {
+                entity.ToTable("BOOKS");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.BookCode)
+                    .HasColumnName("BOOK_CODE")
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.BookCount).HasColumnName("BOOK_COUNT");
+
+                entity.Property(e => e.BookName)
+                    .HasColumnName("BOOK_NAME")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
